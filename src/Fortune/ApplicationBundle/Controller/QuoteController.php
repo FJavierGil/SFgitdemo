9ddc92e67308c564872ebaf27aca564752905b6f
FJavierGil <fjavier.gilr@gmail.com>
2014-10-23 20:59:22 +0200

    Created a Symfony2 Emptier application

---
+++ src/Fortune/ApplicationBundle/Controller/QuoteController.php
@@ -0,0 +1,39 @@
+<?php
+// File: src/Fortune/ApplicationBundle/Controller/QuoteController.php
+
+namespace Fortune\ApplicationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class QuoteController extends Controller
+{
+    public function submitAction(Request $request)
+    {
+        $postedContent = $request->getContent();
+        $postedValues = json_decode($postedContent, true);
+
+		if (empty($postedValues['content'])) {
+            $answer = array('message' => 'Missing required parameter: content');
+
+            return new JsonResponse($answer, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+		$quoteRepository = $this->container->get('fortune_application.quote_repository');
+        $quote = $quoteRepository->insert($postedValues['content']);
+
+        return new JsonResponse($quote, Response::HTTP_CREATED);
+		/**        $answer['quote']['content'] = $postedValues['content'];
+
+        return new JsonResponse($answer, Response::HTTP_CREATED);  **/
+    }
+	
+	public function listAction(Request $request)
+    {
+        $quoteRepository = $this->container->get('fortune_application.quote_repository');
+        $quotes = $quoteRepository->findAll();
+
+        return new JsonResponse($quotes, Response::HTTP_OK);
+    }
+}
---
+++ src/Fortune/ApplicationBundle/Tests/Controller/QuoteControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+// File: src/Fortune/ApplicationBundle/Tests/Controller/QuoteControllerTest.php
+
+namespace Fortune\ApplicationBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class QuoteControllerTest extends WebTestCase
+{
+    private function post($uri, array $data)
+    {
+        $headers = array('CONTENT_TYPE' => 'application/json');
+        $content = json_encode($data);
+        $client = static::createClient();
+        $client->request('POST', $uri, array(), array(), $headers, $content);
+
+        return $client->getResponse();
+    }
+
+    private function get($uri)
+    {
+        $headers = array('CONTENT_TYPE' => 'application/json');
+        $client = static::createClient();
+        $client->request('GET', $uri, array(), array(), $headers);
+
+        return $client->getResponse();
+    }
+
+    public function testSubmitNewQuote()
+    {
+        $response = $this->post('/api/quotes', array('content' => '<KnightOfNi> Ni!'));
+
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+    }
+
+    public function testSubmitEmptyQuote()
+    {
+        $response = $this->post('/api/quotes', array('content' => ''));
+
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
+
+    public function testSubmitNoQuote()
+    {
+        $response = $this->post('/api/quotes', array());
+
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
+
+    public function testListingAllQuotes()
+    {
+        $response = $this->get('/api/quotes');
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+}
